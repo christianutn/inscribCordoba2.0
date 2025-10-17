84bdd8fdedd8a82c911167a670deb508f0f16f29
christian bergero <christian.javier.bergero@gmail.com>
2025-10-17 12:39:16 -0300

    Cambios minimo en registrarAsistiencia

--- backend/src/domains/Asistencia/useCases/RegistrarAsistenciaUseCase.js
+++ backend/src/domains/Asistencia/useCases/RegistrarAsistenciaUseCase.js
@@ -1,15 +1,27 @@
 import CidiService from "../../../services/CidiService.js";
+import AsistenciaRepository from "../core/repositories/AsistenciaRepository.js";
+import AsistenciaService from "../core/services/AsistenciaService.js"
 
 
 
 export default class RegistrarAsistenciaUseCase {
 
-
-
     async ejecutar(cuil, nroEvento, diaAsistencia) {
 
-        // Buscamos el cuil en el listado de inscriptos
+        // Buscar en tabla de asistencia
 
-        // Si el cuil está en este listado entonces creamos y marcamos como asistido
+        // Si existe en tabla de asistencia marcar como asistido
+
+        // Sino existe el cuil en asistencia buscar datos por Cidi
+
+        // Buscamos si existe en la tabla de participantes
+        // Si existe, actualizamos participante
+        // Si no existe creamos nuevo participante
+
+        //Consulta datos de dias para el evento
+
+        // Crear inscripcion y asistencia para el participante
+        
+        // marcamos asistencia para el dia día actual
     }
 }
