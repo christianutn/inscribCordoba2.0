2377652f17ee81ee9a2491662950f2839ee43fa2
christian bergero <christian.javier.bergero@gmail.com>
2025-12-17 08:45:48 -0300

    Se modifica config/database para manejo de variables de entorno

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,19 +8,19 @@ let DB_PASSWORD;
 let DB_HOST;
 let DB_PORT;
 
-if (process.env.ES_RED_GOBIERNO == "si") {
-  DB_NAME = process.env.DB_NAME_GOBIERNO;
-  DB_USER = process.env.DB_USER_GOBIERNO;
-  DB_PASSWORD = process.env.DB_PASSWORD_GOBIERNO;
-  DB_HOST = process.env.DB_HOST_GOBIERNO;
-  DB_PORT = process.env.DB_PORT_GOBIERNO;
-} else if (process.env.ES_RED_GOBIERNO != "si" && (process.env.NODE_ENV == "desarrollo" || process.env.NODE_ENV == "test")) {
+if (process.env.NODE_ENV == "production") {
+  DB_NAME = process.env.DB_NAME_PROD;
+  DB_USER = process.env.DB_USER_PROD;
+  DB_PASSWORD = process.env.DB_PASSWORD_PROD;
+  DB_HOST = process.env.DB_HOST_PROD;
+  DB_PORT = process.env.DB_PORT_PROD;
+} else if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "test") {
   DB_NAME = process.env.DB_NAME_DESARROLLO;
   DB_USER = process.env.DB_USER_DESARROLLO;
   DB_PASSWORD = process.env.DB_PASSWORD_DESARROLLO;
   DB_HOST = process.env.DB_HOST_DESARROLLO;
   DB_PORT = process.env.DB_PORT_DESARROLLO;
-
+ 
 }
 
 
